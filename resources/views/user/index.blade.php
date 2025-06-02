8f6dfc2da27538c0fa45ad37abf33a8a1d7485f4
Rendy HP <102426936+rendyhp@users.noreply.github.com>
2025-06-02 17:07:36 +0700

    Update index.blade.php

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -11,12 +11,12 @@
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 <div class="page-header">
-                    <h2 class="pageheader-title ">User Data</h2>
+                    <h2 class="pageheader-title ">Data User</h2>
                     <div class="page-breadcrumb">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a class="" href="/dashboard">Dashboard</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">User Data</li>
+                                <li class="breadcrumb-item active" aria-current="page">Data User</li>
                             </ol>
                         </nav>
                     </div>
